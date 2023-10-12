69467c709c7a8939885a7f468656f6ea94caa84d
Stanle1 <zonkia@gmail.com>
2023-10-12 19:13:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,26 @@ os.chdir(os.path.abspath(parent_dir))
 folders = sorted(os.listdir())
 
 for folder in folders:
-    if folder in already_done or os.path.isfile(os.path.abspath(folder)) or folder in exceptions:
+    if folder in already_done or folder in exceptions:
         print(folder, "already in list\n---")
+        continue
+
+    if os.path.isfile(os.path.abspath(folder)):
+        #folder is file!
+        result = "Error"
+        print(f"---\nRSYNC file: {folder}")
+        while result != "Ok":
+            result = str(subprocess.run(['rsync', '-vrh', f'/{parent_dir}/{folder}', f'/{destination_dir}/'], check=False).returncode).replace("0", "Ok")
+            if result != "Ok":
+                time.sleep(5)
+        already_done.add(str(folder))
+        with open(f'{script_path}/already_done.txt', 'w', encoding='utf8') as file:
+            file.write(str(already_done))
+
         continue
     os.chdir(os.path.abspath(folder))
     subfolders = sorted(os.listdir())
+
     for subfolder in subfolders:
         if str(folder + "/" + subfolder) in already_done or folder in exceptions or subfolder in exceptions:
             print(folder + "/" + subfolder + " already in list\n---")
@@ -72,4 +87,5 @@ for folder in folders:
     already_done.add(str(folder))
     with open(f'{script_path}/already_done.txt', 'w', encoding='utf8') as file:
         file.write(str(already_done))
+
 
